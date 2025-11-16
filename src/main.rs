848c291b6c1ae5ed637d664383d5710c2747883f
Sam Baskin <sambskn@gmail.com>
2025-11-16 15:12:01 -0700

    working on update to picking still..

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use crate::bird::{
     BirdGenInputTypes, BirdGenInputs, generate_bird_body_mesh, generate_bird_head_mesh,
     get_input_type_string, get_input_value_for_type,
 };
+use accesskit::{Node as Accessible, Role};
 use bevy::{
+    a11y::AccessibilityNode,
     color::palettes::basic::*,
+    input::mouse::{MouseScrollUnit, MouseWheel},
     input_focus::{
         InputDispatchPlugin,
         tab_navigation::{TabGroup, TabNavigationPlugin},
     },
-    picking::hover::Hovered,
+    picking::hover::{HoverMap, Hovered},
     prelude::*,
     ui::InteractionDisabled,
     ui_widgets::{
@@ -76,6 +79,7 @@ fn main() {
             Update,
             (
                 update_slider_values,
+                send_scroll_events,
                 handle_bird_rebuild,
                 update_slider_styles,
                 update_slider_styles2,
@@ -83,8 +87,92 @@ fn main() {
                 update_button_style2,
             ),
         )
+        .add_observer(on_scroll_handler)
         .add_systems(OnEnter(BirdState::Loading), spawn_bird_mesh)
         .run();
+}
+
+/// UI scrolling event.
+#[derive(EntityEvent, Debug)]
+#[entity_event(propagate, auto_propagate)]
+struct Scroll {
+    entity: Entity,
+    /// Scroll delta in logical coordinates.
+    delta: Vec2,
+}
+
+fn on_scroll_handler(
+    mut scroll: On<Scroll>,
+    mut query: Query<(&mut ScrollPosition, &Node, &ComputedNode)>,
+) {
+    let Ok((mut scroll_position, node, computed)) = query.get_mut(scroll.entity) else {
+        return;
+    };
+
+    let max_offset = (computed.content_size() - computed.size()) * computed.inverse_scale_factor();
+
+    let delta = &mut scroll.delta;
+    if node.overflow.x == OverflowAxis::Scroll && delta.x != 0. {
+        // Is this node already scrolled all the way in the direction of the scroll?
+        let max = if delta.x > 0. {
+            scroll_position.x >= max_offset.x
+        } else {
+            scroll_position.x <= 0.
+        };
+
+        if !max {
+            scroll_position.x += delta.x;
+            // Consume the X portion of the scroll delta.
+            delta.x = 0.;
+        }
+    }
+
+    if node.overflow.y == OverflowAxis::Scroll && delta.y != 0. {
+        // Is this node already scrolled all the way in the direction of the scroll?
+        let max = if delta.y > 0. {
+            scroll_position.y >= max_offset.y
+        } else {
+            scroll_position.y <= 0.
+        };
+
+        if !max {
+            scroll_position.y += delta.y;
+            // Consume the Y portion of the scroll delta.
+            delta.y = 0.;
+        }
+    }
+
+    // Stop propagating when the delta is fully consumed.
+    if *delta == Vec2::ZERO {
+        scroll.propagate(false);
+    }
+}
+
+const LINE_HEIGHT: f32 = 40.0;
+
+fn send_scroll_events(
+    mut mouse_wheel_reader: MessageReader<MouseWheel>,
+    hover_map: Res<HoverMap>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut commands: Commands,
+) {
+    for mouse_wheel in mouse_wheel_reader.read() {
+        let mut delta = -Vec2::new(mouse_wheel.x, mouse_wheel.y);
+
+        if mouse_wheel.unit == MouseScrollUnit::Line {
+            delta *= LINE_HEIGHT;
+        }
+
+        if keyboard_input.any_pressed([KeyCode::ControlLeft, KeyCode::ControlRight]) {
+            std::mem::swap(&mut delta.x, &mut delta.y);
+        }
+
+        for pointer_map in hover_map.values() {
+            for entity in pointer_map.keys().copied() {
+                commands.trigger(Scroll { entity, delta });
+            }
+        }
+    }
 }
 
 fn kick_off_bird_load(mut next_bird_state: ResMut<NextState<BirdState>>) {
@@ -102,12 +190,13 @@ fn ui_root(asset_server: &AssetServer) -> impl Bundle {
             left: Val::Px(0.),
             top: Val::Px(0.),
             width: Val::Px(300.),
-            height: Val::Percent(100.),
+            height: Val::Percent(70.),
             flex_direction: FlexDirection::Column,
             padding: UiRect::all(Val::Px(10.)),
             overflow: Overflow::scroll_y(),
             ..default()
         },
+        Pickable {should_block_lower: true, is_hoverable: true},
         BackgroundColor(Color::srgba(0.1, 0.1, 0.1, 0.95)),
         TabGroup::default(),
         UiRoot,
@@ -356,9 +445,12 @@ where
             width: Val::Percent(100.),
             flex_direction: FlexDirection::Column,
             margin: UiRect::vertical(Val::Px(5.)),
+            min_height: px(LINE_HEIGHT),
+            max_height: px(LINE_HEIGHT),
             ..default()
         },
         children![
+            AccessibilityNode(Accessible::new(Role::ListItem)),
             // Label
             (
                 Text::new(get_input_type_string(&input_type)),
@@ -613,18 +705,17 @@ fn update_slider_styles2(
 
 fn update_slider_values(
     res: Res<BirdGenInputs>,
-    mut sliders: Query<(Entity, &mut Slider, &BirdInputSlider)>,
+    mut sliders: Query<(Entity, &BirdInputSlider)>,
     mut commands: Commands,
 ) {
     if res.is_changed() {
-        for (slider_ent, mut slider, bird_slider) in sliders.iter_mut() {
+        for (slider_ent, bird_slider) in sliders.iter_mut() {
             commands
                 .entity(slider_ent)
                 .insert(SliderValue(get_input_value_for_type(
                     &bird_slider.input_type,
                     &res,
                 )));
-            // slider.track_click = res.slider_click;
         }
     }
 }
