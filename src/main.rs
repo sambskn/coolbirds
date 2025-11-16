a55cb7a84ee2e82677e22831d00fdacdf91a080d
Sam Baskin <sambskn@gmail.com>
2025-11-16 00:36:27 -0700

    Working on loading state

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ const BG_COLOR: Color = Color::srgb(0.47, 0.69, 0.48);
 #[derive(Message, Debug)]
 struct RebuildBird;
 
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
+enum BirdState {
+    Loading,
+    BirdVisible,
+}
+
 fn main() {
     App::new()
         .add_plugins(
@@ -26,10 +32,12 @@ fn main() {
         .add_plugins(EguiPlugin::default())
         .add_plugins(PanOrbitCameraPlugin)
         .add_message::<RebuildBird>()
+        .insert_state(BirdState::Loading)
         .insert_resource(ClearColor(BG_COLOR))
         .insert_resource(BirdGenInputs::default())
         .add_systems(Startup, (spawn_camera_and_light, spawn_bird_mesh))
-        .add_systems(Update, (camera_update, handle_bird_rebuild))
+        .add_systems(Update, handle_bird_rebuild)
+        .add_systems(OnEnter(BirdState::Loading), spawn_bird_mesh)
         .add_systems(EguiPrimaryContextPass, ui_example_system)
         .run();
 }
@@ -38,6 +46,8 @@ fn ui_example_system(
     mut contexts: EguiContexts,
     mut bird_inputs: ResMut<BirdGenInputs>,
     mut remake_the_bird: MessageWriter<RebuildBird>,
+    mut next_bird_state: ResMut<NextState<BirdState>>,
+    bird_state: Res<State<BirdState>>,
 ) -> Result {
     egui::SidePanel::left("left_panel")
         .resizable(true)
@@ -126,9 +136,15 @@ fn ui_example_system(
                 // );
 
                 ui.separator();
-                if ui.button("regenerate bird").clicked() {
-                    remake_the_bird.write(RebuildBird);
+                if *bird_state.get() == BirdState::BirdVisible {
+                    if ui.button("regenerate bird").clicked() {
+                        remake_the_bird.write(RebuildBird);
+                        next_bird_state.set(BirdState::Loading);
+                    }
+                } else {
+                    ui.label("generating bird...");
                 }
+                ui.separator();
                 ui.label("ported/inspired by bird-o-matic by mooncactus");
             });
             ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
@@ -143,34 +159,12 @@ fn handle_bird_rebuild(
     mut bird_rebuild_message: MessageReader<RebuildBird>,
     bird_mesh_query: Query<Entity, With<BirdMesh>>,
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    bird_inputs: Res<BirdGenInputs>,
 ) {
-    let current_bird_inputs = bird_inputs.into_inner();
     for _bird_rebuild_event in bird_rebuild_message.read() {
         // kill all bird meshes
         for brid_mesh_entity in bird_mesh_query {
             commands.entity(brid_mesh_entity).despawn();
         }
-        // now make new ones and spawn em
-        let basic_material = materials.add(StandardMaterial {
-            base_color: Color::srgb(0.83, 0.26, 0.17),
-            ..default()
-        });
-
-        commands.spawn((
-            Mesh3d(meshes.add(generate_bird_head_mesh(current_bird_inputs))),
-            MeshMaterial3d(basic_material.clone()),
-            Transform::from_xyz(0.0, 0.0, 0.0),
-            BirdMesh,
-        ));
-        commands.spawn((
-            Mesh3d(meshes.add(generate_bird_body_mesh(current_bird_inputs))),
-            MeshMaterial3d(basic_material),
-            Transform::from_xyz(0.0, 0.0, 0.0),
-            BirdMesh,
-        ));
     }
 }
 
@@ -181,6 +175,7 @@ fn spawn_bird_mesh(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut next_bird_state: ResMut<NextState<BirdState>>,
     bird_inputs: Res<BirdGenInputs>,
 ) {
     let basic_material = materials.add(StandardMaterial {
@@ -200,12 +195,13 @@ fn spawn_bird_mesh(
         Transform::from_xyz(0.0, 0.0, 0.0),
         BirdMesh,
     ));
+    next_bird_state.set(BirdState::BirdVisible);
 }
 
 fn spawn_camera_and_light(mut commands: Commands) {
     // spawn camera
     commands.spawn((
-        PanOrbitCamera::default(),        
+        PanOrbitCamera::default(),
         Transform::from_xyz(65.0, 40.0, 65.0).with_rotation(quat(
             -0.07382465,
             0.46779895,
@@ -225,66 +221,4 @@ fn spawn_camera_and_light(mut commands: Commands) {
             -0.2638357, 0.52681506, 0.1762679, 0.7885283,
         )),
     ));
-}
-
-const CAM_SENSITIVITY_X: f32 = 1.1;
-const CAM_SENSITIVITY_Y: f32 = 0.7;
-const SPEED: f32 = 12.0;
-
-fn camera_update(
-    camera_transform: Query<&mut Transform, With<Camera3d>>,
-    gamepads: Query<&Gamepad>,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    timer: Res<Time>,
-) {
-    for mut cam in camera_transform {
-        for gamepad in gamepads {
-            // get unscaled stick inputs + dpad
-            let l_stick = gamepad.left_stick();
-            let r_stick = gamepad.right_stick();
-            let d_pad = gamepad.dpad();
-            // also keyboard i guess *eyeroll*
-            let kb_wasd = Vec2::new(
-                if keyboard_input.pressed(KeyCode::KeyD) {
-                    1.0
-                } else if keyboard_input.pressed(KeyCode::KeyA) {
-                    -1.0
-                } else {
-                    0.0
-                },
-                if keyboard_input.pressed(KeyCode::KeyW) {
-                    1.0
-                } else if keyboard_input.pressed(KeyCode::KeyS) {
-                    -1.0
-                } else {
-                    0.0
-                },
-            );
-
-            // movement
-            let combined_stick_magnitude = l_stick.length() + d_pad.length() + kb_wasd.length();
-            if combined_stick_magnitude > 0.1 {
-                let combined_movement_intent = (l_stick + d_pad + kb_wasd).normalize();
-                let move_vec = combined_movement_intent * SPEED * timer.delta_secs();
-
-                let offset = move_vec.x * cam.local_x() + move_vec.y * -1.0 * cam.local_z();
-                cam.translation += offset;
-            }
-
-            // camera
-            if r_stick.length() > 0.1 {
-                let mut cam_adjust = r_stick;
-                cam_adjust.x *= CAM_SENSITIVITY_X;
-                cam_adjust.y *= CAM_SENSITIVITY_Y;
-                cam.rotate_y(-1.0 * cam_adjust.x * timer.delta_secs());
-                cam.rotate_local_x(cam_adjust.y * timer.delta_secs());
-            }
-
-            // debug prints
-            if gamepad.just_pressed(GamepadButton::South) {
-                // print out current camera tf
-                info!("camera tf {:?}", cam);
-            }
-        }
-    }
 }
